8a37277d90b2b878a6b1b8015c80fe6fdf24a4ac
Tom Settle <tom.k.settle@gmail.com>
2023-02-03 10:35:48 -0600

    Addressed all the TODO's

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -5,10 +5,10 @@ namespace ChristmasWithTypes
     {
         public string[] Presents;
         public string Santa { get; set; }
-        public int TreeHeight { get; set; } //TODO Make the Height property nullable
+        public int? TreeHeight { get; set; } //DONE - TODO Make the Height property nullable
 
-        //TODO Make the property, "Day", type enum
-        public enum Day { /*Fill out the days of the week*/ };
+        //DONE - TODO Make the property, "Day", type enum
+        public enum Day { Sun, Mon, Tue, Wed, Thu, Fri, Sat };
 
     }
 }
--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -8,16 +8,16 @@ namespace ChristmasWithTypes
         {
             var xmas = new Christmas();
 
-            var xmasDay = Christmas.Day.Thursday;
+            var xmasDay = Christmas.Day.Thu;        //Changed this to the style of day names I put into the enum
 
-            //TODO set Santa's name to Kris Kringle
-            xmas.Santa = null;
+            //DONE - TODO set Santa's name to Kris Kringle
+            xmas.Santa = "Kris Kringle";
 
-            //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{ }; 
+            //DONE - TODO Insert 3 presents you would like for xmas.  They must be strings
+            xmas.Presents =  new string[3]{"Hot Wheels", "Hiking Boots", "Switch game" }; 
 
-            //TODO Set the TreeHeight to 10
-            xmas.TreeHeight = null;
+            //DONE - TODO Set the TreeHeight to 10
+            xmas.TreeHeight = 10;
 
             Console.WriteLine($"This year christmas falls on {xmasDay} \n");
             Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
